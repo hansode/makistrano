6cbde9f5bb7281b918148a17ffc9d3acd27e33b7
Masahito Yoshida <masahito@axsh.net>
2013-10-01 23:01:28 +0900

    test: add empty node case.

--- test/unit/makistrano/t.makistrano_nodes.sh
+++ test/unit/makistrano/t.makistrano_nodes.sh
@@ -13,7 +13,13 @@
 ## functions
 
 function test_makistrano_nodes() {
-  makistrano_nodes hoge >/dev/null
+  makistrano_nodes arg
+  assertEquals 0 $?
+}
+
+function test_makistrano_nodes_empty_nodes() {
+  function nodes() { echo; }
+  makistrano_nodes arg
   assertEquals 0 $?
 }
 
